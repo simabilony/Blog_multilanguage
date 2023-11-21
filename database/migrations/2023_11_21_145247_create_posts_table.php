99ff2dfebd5328fc16eea0f6b6a179bf2714538d
GWTN156-4\sima <www.simabilony.com>
2023-11-22 00:10:35 +0300

    post_translations

--- database/migrations/2023_11_21_145247_create_posts_table.php
+++ database/migrations/2023_11_21_145247_create_posts_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('author');
+            $table->integer('user_id')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
